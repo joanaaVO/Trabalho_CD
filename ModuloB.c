9b3f2b75f8d7d0c5f9cb9a9c27731a144fb648bb
joanaaVO <joanaveigaoliveira@gmail.com>
2020-12-20 23:00:44 +0000

    Update ModuloB.c

--- ModuloB.c
+++ ModuloB.c
@@ -10,12 +10,26 @@
 //nesse caso avanca para o proximo bloco
 //1ºciclo(para o 1ºbloco) é diferente dos restantes devido ao inicio do array
 
-char *ordena (char freq[]) {
-    int a = 4; //número de @
+//função que guarda o valor,bloco, o código SF e a frequência
+//criar uma struct para tal
+
+typedef struct inf {
+    int valor;
+    int bloco;
+    char *sf; //código SF
+    int freq;
+    struct inf *prox;
+} *Inf;
+
+//cria uma lista nova com os valores das freq de cada bloco
+int *freqsbloco (char freq[], int ind) { //indíce do bloco (número de @-1 a percorrer após os 4 primeiros)
+    int a = 4 + ind - 1; //número de @ a percorrer (4 iniciais + ind - 1)
     int i = 0; //percorrer os blocos
     int j; //percorrer as frequências dos símbolos de cada bloco
+    int *l; 
+    int n; //nº de caracteres da frequência
 
-    while (a>0) { //serve para avançar com o array até ao sítio onde começam as frequências (após o 4 @) 
+    while (a>0) { //serve para avançar com o array até ao sítio onde começa o bloco 
         if (freq[i]=='@') {
             a--;
             i++;
@@ -23,32 +37,71 @@ char *ordena (char freq[]) {
         else i++;
     }
 
-    for ( ; freq[i]!='0'; i++) { //percorre o array todo
-        for (j=i; freq[j]!='@'; j++) { //percorre cada bloco
-            if (valor(freq,j) < valor )
+    for ( ; freq[i]!= '@'; i+=n) { //percorre o bloco
+        n = 0;
+        for (j=i; freq[j]!=';'; j++) n++; //percorre cada frequência
+        strcat(l, valor(freq,j,n));
         }
-    }
-
-    
+        return l; 
 }
 
 //retira o valor da frequência e transforma-o num inteiro
-int valor (char freq[], int ind) {
-    int n = 0; //nº de carateres
-    int i;
+int valor (char freq[], int i, int n) {
     char *str;
 
-    for (i=ind; freq[i]!=';'; i++) n++;
-    strncpy (str, &freq[i-n],n);
+    strncpy (str, &freq[i],n);
     int x = atoi(str);
 
     return x;    
 } 
 
+char *calcular_codigos_SF (char freq[]) {
+    char *codes;
+    int i = 0;
+    int j;
+    int end = strlen(freq)-1;
+    int div;
+    int divAnt;
+
+    for (i=0; ... ; i++) {
+        div = calcular_melhor_divisao(freq,i,end);
+        for (j=i; j<=div; j++) strcat(codes,'0');
+        for (j=div+1; j<=end; j++) strcat(codes,'1');
+        j=i; //(?)
+        divAnt = div;
+
+        while (j!=divAnt) { //ou div?
+            if (divAnt > div) { //>= (?)
+                divAnt = div; //guarda a divisão anterior
+                div = calcular_melhor_divisao(freq,j,div);
+            }
+            else divAnt = end;
+            int k;
+            for (k=j; k<=div; k++) strcat(codes,'0');
+            for (k=div+1; k<=divAnt; k++) strcat(codes,'1');
+            // assim j vai ser sempre !=div ?? v
+            div = calcular_melhor_divisao(freq,divAnt+1,end); //(?)
+            j = divAnt+1;
+            //colocar isto no início fora do ciclo ? e fazer as devidas alterações (??)
+        }
+        div = calcular_melhor_divisao(freq,divAnt+1,end);
+        for(j= divAnt+1; j<=div; j++) strcat(codes,'0');
+        for (j=div+1; j<=end; j++) strcat(codes,'1');
+
+
+
+
+
+    }
+
+    
+
+}
+
 int main () {
     int k;
     char s[] = "@R@2@57444@1322;335;";
-    k = valor(s,11);
+    k = valor(s,11,4);
     printf ("Resultado: %i\n", k);
 
     return 0;
